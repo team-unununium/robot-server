ae4da16ff06ff0db3d19a6b35fadb0e88c382473
PC Chin <chin.pengcheng@gmail.com>
2020-01-18 19:35:02 +0800

    First round complete

--- app.js
+++ app.js
@@ -1,9 +1,110 @@
-express = require('express')
+// In-built modules
+require('events').EventEmitter.defaultMaxListeners = 15;
+const http = require('http')
+
+// npm modules
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const socketio = require('socket.io')
+const socketauth = require('socketio-auth')
+
+// User defined modules
+require('./utils/mongoose')
+const AppClient = require('./models/AppClient')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-app.listen()
-const port = process.env.PORT || 5500
+const port = process.env.PORT
+
+// Socket.io Authentication
+socketauth(io, {
+    authenticate: (socket, data, callback) => {
+        const guid = data.guid
+        const token = data.token
+
+        if (guid === '' && token === process.env.SERVER_ROBOT_SECRET) {
+            return callback(null, true)
+        }
+
+        AppClient.findOne({guid, token}, (e, client) => {
+            if (e || !client) {
+                callback(null, false)
+            } else {
+                callback(null, true)
+            }
+        })
+    },
+    timeout: 5000
+})
+
+io.on('connection', (socket) => {
+    const receiveDataCallback = (e, data) => {
+        if (!e) {
+            socket.broadcast.emit('dataReceived', data)
+        }
+    }
+
+    // Called from client and robot side
+    socket.on('join', (type, callback) => {
+        if (type !== 'robot' && type !== 'client') {
+            return callback(new Error('Invalid params'))
+        }
+
+        socket['socketType'] = type
+        callback()
+
+        if (type === 'robot') {
+            // Check if there is another robot, if yes, kick that one out
+            io.sockets.clients((error, clients) => {
+                if (!error) {
+                    clients.forEach((client) => {
+                        if (client['socketType'] === 'robot') {
+                            client.disconnect(true)
+                        }
+                    })
+                }
+            })
+
+            // Get initial data if the client is a robot
+            socket.emit('requestData', null, receiveDataCallback)
+        }
+    })
+
+    // Only called from client side
+    socket.on('getBot', (data, callback) => {
+        const clientList = io.sockets.clients()
+        io.sockets.clients((error, clients) => {
+            if (!error) {
+                clients.forEach((client) => {
+                    if (client['socketType'] === 'robot') {
+                        return callback(true)
+                    }
+                })
+            }
+        })
+        callback(false)
+    })
+
+    // Only called from robot side
+    socket.on('updateData', (data, callback) => {
+        socket.broadcast.emit('dataReceived', data)
+    })
+})
+
+// Get token using GUID and common secret
+app.post('/access', async (req, res) => {
+    if (!req.guid || !req.secret || req.secret !== process.env.SERVER_CLIENT_SECRET) {
+        return res.status(400).send()
+    }
+
+    // Overwrite existing client data
+    const client = new AppClient({ guid: req.guid, token: jwt.sign({ _id: req.guid })})
+    await client.save()
+
+    res.status(201).send(client)
+})
 
 // Connection Test
 app.get('/test', (req, res) => {
---
+++ models/AppClient.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const appClientSchema = new mongoose.Schema({
+    guid: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    token: {
+        type: String,
+        required: true
+    }
+})
+
+const AppClient = mongoose.model('AppClient', appClientSchema)
